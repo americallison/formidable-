6daa1f0eed120c08e7224c101519756e374b7d6f
Americ Allison <96029332+americallison@users.noreply.github.com>
2022-12-12 14:45:21 +0000

    Home.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./style.css";
 import "./App.scss";
-
+import Home from "./components/Home.js"
 
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home () {
+    return (
+        <>
+            <h1>Create forms easily</h1>
+            <h5>Create forms easily to collect meaningful data and scale your business and school.</h5>
+            <div>
+                <button>Create Form</button>
+            </div>
+        </>
+    )
+}
